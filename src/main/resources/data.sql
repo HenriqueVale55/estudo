facc21c8471a4fff403ad92e7352ea2cd4c51de8
Henrique <henriquevale55@gmail.com>
2022-08-22 15:43:41 -0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Usuarios (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(255), idade INT);
+INSERT INTO Usuarios (nome, idade) VALUES('Henrique',21);
+INSERT INTO Usuarios (nome, idade) VALUES('Bruno', 32);
+INSERT INTO Usuarios (nome, idade) VALUES('Patrícia', 50);
+INSERT INTO Usuarios (nome, idade) VALUES('Gabryelly', 23);
+INSERT INTO Usuarios (nome, idade) VALUES('Francisco', 48);
+INSERT INTO Usuarios (nome, idade) VALUES('Eduardo', 62);
+INSERT INTO Usuarios (nome, idade) VALUES('Antônio', 60);
+INSERT INTO Usuarios (nome, idade) VALUES('Otávio', 16);
